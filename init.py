ff7e36b2916412026df340453dae9c1c20de014a
eshadatta-crossref <edatta@crossref.org>
2023-06-14 09:55:20 -0400

    adding sub command for doi prefix currently

--- init.py
+++ init.py
@@ -23,23 +23,26 @@ def set_args():
     default_filename = s.default_config_filename
     """CLI"""
     parser = argparse.ArgumentParser(
-                    description="Generate a permanent ID for a specific file")
+                    description="Generate a permanent ID for a static site generator")
     # if json file does not exist, one will be created
     parser.add_argument('-p', '--pid-file-path', help='Path to json file where containing all the information associated with the files and their permanent IDs; relative to the repository root. If the file does not exist, a new file with the specified filename will be created', required=True)
-    parser.add_argument('-c', '--content', required=True, type=str, nargs='+', help="Examples: -c filepath1 filepath2; relative to the repository root")
-    parser.add_argument('-r', '--repo-path', help='Path to repository containing the files', type=lambda s:check_path(parser,s, "dir"), required=True)
     parser.add_argument('-cf', '--config-filename', nargs='?', type=str, default = default_filename, help='Filename for config init, has a default filename if none is specified')
     parser.add_argument('-b', '--branch', help='Path to branch where the file is located. The default is the active branch of the repository')
-    parser.add_argument('-doi', '--doi-prefix', help='DOI prefix of the member registering this content', required=True)
     parser.add_argument('-d', '--domain', help='Production domain of the content, for example: https://www.crossref.org/', required=True)
     #parser.add_argument('-sd', '--staging-domain', help='Staging domain of the content, for example: https://www.crossref.org/', required=True)
     #parser.add_argument('-ld', '--local-domain', help='Localhost domain of the content, for example: https://www.crossref.org/', required=True)
+    parser.add_argument('-doi', '--doi', action='store_true', required=True, help='Identifier type, in this case doi')
     parser.add_argument('-dry', '--dry-run', help='Dry run to generate a permanent ID of a specified file', action='store_true')
-    
-    args = parser.parse_args()
+    id_types, _ = parser.parse_known_args()
+    idtype_parser = argparse.ArgumentParser(
+        description="Generate a permanent ID for a static site generator", parents=[parser], add_help=False)
+    idtype_parser.add_argument('--doi-prefix', required=id_types.doi, help="Add doi prefix string to this argument, example: --doi-prefix 'x.xxx'")
+    args = idtype_parser.parse_args()
     return args
-
+    
 def main():
+    set_args()
+    exit()
     args = set_args()
     print(args.content)
     c = cf.ConfigFile(args.repo_path, args.content, args.pid_file_path, args.config_filename)
